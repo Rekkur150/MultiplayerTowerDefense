788202803aada77d64673f51aa1ab516b019ee5e
Michael Nickels <mcnickels55@gmail.com>
2021-04-15 11:40:41 -0400

    MainMenuButtons
    Can now enter and exit options menu. Can exit game by hitting exit (only possible by when game is built and then ran)

---
+++ Assets/Capstone/Scripts/Menus/OptionsMenuEnter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenuEnter : MonoBehaviour
+{
+    public void Activate()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void DeActivate()
+    {
+        gameObject.SetActive(false);
+    }
+
+    
+}
